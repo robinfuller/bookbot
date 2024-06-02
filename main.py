b8f566f35c7f7b37a3d4d3b4182b2b594b3dd506
robinfuller <robinc.fuller@gmail.com>
2024-06-02 11:59:16 -0600

    clean up spacing between funcs

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ def main():
     path = "books/frankenstein.txt"
     words = get_words(path)
     num_words = get_word_count(words)
+    char_dict  = get_character_count(words)
     print(f"{num_words} found found @ {path}")
-   
-    
+    print(char_dict)
+
+
 def get_words(path):
     with open(path) as f:
         return f.read()
@@ -15,5 +17,21 @@ def get_words(path):
 def get_word_count(book: str) -> int:
     words = book.split()
     return len(words)
+
+
+
+def get_character_count(book: str):
+    character_dict = {}
+    words = book.split()
+    occurence = 1
+    for word in words:
+        word = word.lower()
+        for letter in word:
+            if letter in character_dict:
+                character_dict[letter] +=1
+            else:
+                character_dict[letter] = 1
+    return character_dict
+
 
 main()
